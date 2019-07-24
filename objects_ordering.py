40044126c2e93cd597349b66d3d1614e8b371ca3
Erick Laureano <erickllaureano@gmail.com>
2019-07-24 02:08:39 -0500

    se agrega la función la cual permite eliminar los pares de clave-valor que anteriormente contenían la información de los hijos

--- objects_ordering.py
+++ objects_ordering.py
@@ -14,5 +14,19 @@ def firts_transform_data(data):
     return aux_list
 
 
+def deleted_old_elements(data_list):
+    accepted_keys = ['name', 'level', 'priority', 'childs']
+    for item_list in data_list:
+        aux_list = list()
+        for item in item_list:
+            if item not in accepted_keys:
+                aux_list.append(item)
+        for delet_object in aux_list:
+            del(item_list[delet_object])
+        if len(item_list['childs']) > 0:
+            childs = deleted_old_elements(item_list['childs'])
+            item_list['childs'] = childs
+    return data_list
+
 
 # Funcion para ejecución individual
