8984a5bfb926e3552ce1b84f05ee58ea49aa223a
Erick Laureano <erickllaureano@gmail.com>
2019-07-23 23:26:09 -0500

    se agrega la primer funcion auxiliar la cual retorna la siguiente vocal en forma circular.

--- change_vowels.py
+++ change_vowels.py
@@ -3,7 +3,29 @@ vowels = list('aeiouAEIOU')
 
 
 # Funciones auxiliares
+def next_vowels(vowel):
+    """
+    Funcion para recibir una vocal
+    y retornar la siguiente vocal
+    respetando ser caseSensitive
 
+    :param vowel: string
+    :return: string
+    """
+    index = 0
+
+    # Ciclo para encontrar le indice de la vocal indicada
+    for letter in vowels:
+        if letter == vowel:
+            break
+        index += 1
+
+    # retorno de la siguiente vocal
+    if index == 4:
+        return vowels[0]
+    if index == 9:
+        return vowels[5]
+    return vowels[index + 1]
 
 
 # Funcion para ejecución individual
